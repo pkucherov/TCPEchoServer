698bdc4771b03deb1dd2bf546d7d62a644ed18d9
Pavel Kucherov <kucherov@sqlmaze.com>
2014-12-23 13:03:34 +0300

    ServerAddresses parser implementation started

--- TCPEchoClient/Program.cs
+++ TCPEchoClient/Program.cs
@@ -7,14 +7,39 @@ namespace TCPEchoClient
     {
         static void Main(string[] args)
         {
+            ParametersParser parser = new ParametersParser(args);
+            parser.GetEndPoints();
+            
+
             EchoClient echoClient = new EchoClient();
             int nPort = 2030;
-            echoClient.Connect(new IPEndPoint(IPAddress.Parse("192.168.1.3"), nPort));
+            echoClient.Connect(new IPEndPoint(IPAddress.Parse("192.168.0.155"), nPort));
             for (; ; )
             {
                 string strData = Console.ReadLine();
                 echoClient.SendData(strData);
             }
+        }
+    }
+
+    class ParametersParser
+    {
+        public ParametersParser(string[] args)
+        {
+            foreach (string serverAddress in args)
+            {
+                parseAddress(serverAddress);
+            }
+        }
+
+        private void parseAddress(string serverAddress)
+        {
+            
+        }
+
+        public IPEndPoint[] GetEndPoints()
+        {
+            return null;
         }
     }
 }
