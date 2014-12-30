1c5e1dc28cf995c49c5adc775f563cbd6dd1ff98
Pavel Kucherov <pavel.kucherov@gmail.com>
2014-12-30 23:05:12 +0300

    unit tests were added

--- EchoUnitTest/EchoUnitTest.cs
+++ EchoUnitTest/EchoUnitTest.cs
@@ -12,10 +12,10 @@ using TCPEchoServer;
 namespace EchoUnitTest
 {
     [TestClass]
-    public class EchoUnitTest
+    public class EchoUnitTestMultiTask
     {
-        [TestMethod]
-        public void SendReceiveEcho()
+        //[TestMethod]
+        public void SingleClient()
         {
             ManualResetEvent exitEvent = new ManualResetEvent(false);
             ManualResetEvent serverStarted = new ManualResetEvent(false);
@@ -31,7 +31,7 @@ namespace EchoUnitTest
             Task taskClient = Task.Run(() =>
             {
                 List<IPEndPoint> endPoints = new List<IPEndPoint>();
-                IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"),2030);
+                IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2030);
                 endPoints.Add(ep);
                 EchoTestClient echoClient = new EchoTestClient(endPoints);
 
@@ -47,8 +47,104 @@ namespace EchoUnitTest
                 exitEvent.Set();
             });
             taskServer.Wait();
-            taskClient.Wait();
-            
+            taskClient.Wait();            
+        }
+    }
+    [TestClass]
+    public class EchoUnitTest
+    {
+     
+
+        [TestMethod]
+        public void SingleThreadSingleClient()
+        {
+            EchoServer echoServer = new EchoServer();
+            echoServer.Start(new IPEndPoint(IPAddress.Any, 2031));
+
+            List<IPEndPoint> endPoints = new List<IPEndPoint>();
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2031);
+            endPoints.Add(ep);
+            EchoTestClient echoClient = new EchoTestClient(endPoints);
+
+            echoClient.Connect(endPoints[0]);
+            string strTestData = "1234567890AaBbCcDdEeFfGgHhJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+            echoClient.SendData(strTestData);
+            echoClient.DataReady.WaitOne();
+            Assert.IsTrue(string.Equals(strTestData, echoClient.ReceivedData));
+            string strTestData2 = "JjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890AaBbCcDdEeFfGgHhJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890AaBbCcDdEeFfGgHh";
+            echoClient.SendData(strTestData2);
+            echoClient.DataReady.WaitOne();
+            Assert.IsTrue(string.Equals(strTestData2, echoClient.ReceivedData));
+        }
+
+        // [TestMethod]
+        public void MultipleClients()
+        {
+            ManualResetEvent exitEvent = new ManualResetEvent(false);
+            ManualResetEvent serverStarted = new ManualResetEvent(false);
+            Task taskServer = Task.Run(() =>
+            {
+                EchoServer echoServer = new EchoServer();
+                echoServer.Start(new IPEndPoint(IPAddress.Any, 2032));
+                serverStarted.Set();
+                exitEvent.WaitOne();
+            });
+
+            serverStarted.WaitOne();
+
+            List<IPEndPoint> endPoints = new List<IPEndPoint>();
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2032);
+            endPoints.Add(ep);
+            List<EchoTestClient> aClients = new List<EchoTestClient>();
+            for (int i = 0; i < 10; i++)
+            {
+                EchoTestClient echoClient = new EchoTestClient(endPoints);
+                echoClient.Connect(endPoints[0]);
+                aClients.Add(echoClient);
+            }
+
+            Task taskClient = Task.Run(() =>
+            {
+                EchoTestClient echoClient = aClients[0];
+                string strTestData = "1234567890AaBbCcDdEeFfGgHhJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+                echoClient.SendData(strTestData);
+                echoClient.DataReady.WaitOne();
+                Assert.IsTrue(string.Equals(strTestData, echoClient.ReceivedData));
+                string strTestData2 = "JjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890AaBbCcDdEeFfGgHhJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890AaBbCcDdEeFfGgHh";
+                echoClient.SendData(strTestData2);
+                echoClient.DataReady.WaitOne();
+                Assert.IsTrue(string.Equals(strTestData2, echoClient.ReceivedData));
+                exitEvent.Set();
+            });
+
+        }
+        [TestMethod]
+        public void MultipleClientsSingleThread()
+        {
+            EchoServer echoServer = new EchoServer();
+            echoServer.Start(new IPEndPoint(IPAddress.Any, 2032));
+
+            List<IPEndPoint> endPoints = new List<IPEndPoint>();
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2032);
+            endPoints.Add(ep);
+            List<EchoTestClient> aClients = new List<EchoTestClient>();
+            for (int i = 0; i < 10; i++)
+            {
+                EchoTestClient echoClient = new EchoTestClient(endPoints);
+                echoClient.Connect(endPoints[0]);
+                aClients.Add(echoClient);
+            }
+
+            EchoTestClient clientMaster = aClients[0];
+            string strTestData2 = "JjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890AaBbCcDdEeFfGgHhJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890AaBbCcDdEeFfGgHh";
+            clientMaster.SendData(strTestData2);
+
+            for (int i = 1; i < 10; i++)
+            {
+                EchoTestClient echoClient = aClients[i];
+                echoClient.DataReady.WaitOne();
+                Assert.IsTrue(string.Equals(strTestData2, echoClient.ReceivedData));
+            }
         }
     }
 
@@ -60,13 +156,13 @@ namespace EchoUnitTest
         public EchoTestClient(List<IPEndPoint> endPoints)
             : base(endPoints)
         {
-            
+
         }
         protected override void onDataPacketReaded(SocketAsyncEventArgs args, IDataPacket dp)
         {
-            ReceivedData = ((DataPacket) dp).Data;
+            ReceivedData = ((DataPacket)dp).Data;
             DataReady.Set();
         }
-        
+
     }
 }
