a2ceced7b958ad38fe0c1e2ba5064cb59a72d381
Pavel Kucherov <pavel.kucherov@gmail.com>
2015-01-02 15:41:55 +0300

    unit tests

--- EchoUnitTest/EchoUnitTest.cs
+++ EchoUnitTest/EchoUnitTest.cs
@@ -11,50 +11,10 @@ using TCPEchoServer;
 
 namespace EchoUnitTest
 {
-    [TestClass]
-    public class EchoUnitTestMultiTask
-    {
-        //[TestMethod]
-        public void SingleClient()
-        {
-            ManualResetEvent exitEvent = new ManualResetEvent(false);
-            ManualResetEvent serverStarted = new ManualResetEvent(false);
-            Task taskServer = Task.Run(() =>
-            {
-                EchoServer echoServer = new EchoServer();
-                echoServer.Start(new IPEndPoint(IPAddress.Any, 2030));
-                serverStarted.Set();
-                exitEvent.WaitOne();
-            });
-
-            serverStarted.WaitOne();
-            Task taskClient = Task.Run(() =>
-            {
-                List<IPEndPoint> endPoints = new List<IPEndPoint>();
-                IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2030);
-                endPoints.Add(ep);
-                EchoTestClient echoClient = new EchoTestClient(endPoints);
-
-                echoClient.Connect(endPoints[0]);
-                string strTestData = "1234567890AaBbCcDdEeFfGgHhJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
-                echoClient.SendData(strTestData);
-                echoClient.DataReady.WaitOne();
-                Assert.IsTrue(string.Equals(strTestData, echoClient.ReceivedData));
-                string strTestData2 = "JjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890AaBbCcDdEeFfGgHhJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890AaBbCcDdEeFfGgHh";
-                echoClient.SendData(strTestData2);
-                echoClient.DataReady.WaitOne();
-                Assert.IsTrue(string.Equals(strTestData2, echoClient.ReceivedData));
-                exitEvent.Set();
-            });
-            taskServer.Wait();
-            taskClient.Wait();            
-        }
-    }
+   
     [TestClass]
     public class EchoUnitTest
     {
-     
-
         [TestMethod]
         public void SingleThreadSingleClient()
         {
@@ -77,47 +37,6 @@ namespace EchoUnitTest
             Assert.IsTrue(string.Equals(strTestData2, echoClient.ReceivedData));
         }
 
-        // [TestMethod]
-        public void MultipleClients()
-        {
-            ManualResetEvent exitEvent = new ManualResetEvent(false);
-            ManualResetEvent serverStarted = new ManualResetEvent(false);
-            Task taskServer = Task.Run(() =>
-            {
-                EchoServer echoServer = new EchoServer();
-                echoServer.Start(new IPEndPoint(IPAddress.Any, 2032));
-                serverStarted.Set();
-                exitEvent.WaitOne();
-            });
-
-            serverStarted.WaitOne();
-
-            List<IPEndPoint> endPoints = new List<IPEndPoint>();
-            IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2032);
-            endPoints.Add(ep);
-            List<EchoTestClient> aClients = new List<EchoTestClient>();
-            for (int i = 0; i < 10; i++)
-            {
-                EchoTestClient echoClient = new EchoTestClient(endPoints);
-                echoClient.Connect(endPoints[0]);
-                aClients.Add(echoClient);
-            }
-
-            Task taskClient = Task.Run(() =>
-            {
-                EchoTestClient echoClient = aClients[0];
-                string strTestData = "1234567890AaBbCcDdEeFfGgHhJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
-                echoClient.SendData(strTestData);
-                echoClient.DataReady.WaitOne();
-                Assert.IsTrue(string.Equals(strTestData, echoClient.ReceivedData));
-                string strTestData2 = "JjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890AaBbCcDdEeFfGgHhJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890AaBbCcDdEeFfGgHh";
-                echoClient.SendData(strTestData2);
-                echoClient.DataReady.WaitOne();
-                Assert.IsTrue(string.Equals(strTestData2, echoClient.ReceivedData));
-                exitEvent.Set();
-            });
-
-        }
         [TestMethod]
         public void MultipleClientsSingleThread()
         {
